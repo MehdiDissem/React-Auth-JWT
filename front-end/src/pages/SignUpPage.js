fada43444bd7888d9100a017d5c6192185a6c466
Mehdi Dissem <mehdissem@gmail.com>
2023-04-18 23:13:48 +0200

    login signup done

---
+++ front-end/src/pages/SignUpPage.js
@@ -0,0 +1,26 @@
+import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
+
+export const SignUpPage=()=>{
+    const [errorMessage,setErrorMessage]=useState('')
+    const [emailValue,setEmailValue]=useState('')
+    const [passwordValue,setPasswordValue]=useState('')
+    const [confirmPasswordValue,setConfirmPasswordValue]=useState('')
+    const history = useHistory()
+
+    const onSignUpClicked = async ()=>{
+        alert('log in not implemented yet')
+    }
+
+    return (
+        <div className="content-container">
+            <h1>Log In</h1>
+            {errorMessage && <div className="fail">{errorMessage}</div>}
+            <input value={emailValue} onChange={e=> setEmailValue(e.target.value)} placeholder ="someone@gmail.com"/>
+            <input value ={passwordValue} onChange = {e=>setPasswordValue(e.target.value)} type="password" placeholder="password"/>
+            <input value ={confirmPasswordValue} onChange = {e=>setConfirmPasswordValue(e.target.value)} type="confirm password" placeholder="password"/>
+            <button disabled ={!emailValue || !passwordValue || passwordValue !== confirmPasswordValue} onClick={onSignUpClicked}>Sign Up</button>
+            <button onClick={()=> history.push('/login')}>Already have an account</button>
+        </div>
+    )
+}
