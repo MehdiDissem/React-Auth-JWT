981eb5614d645200ec5ffdd0ffaed8ae5186a3a0
Mehdi Dissem <mehdissem@gmail.com>
2023-04-18 23:22:36 +0200

    private route

--- front-end/src/pages/LoginPage.js
+++ front-end/src/pages/LoginPage.js
@@ -17,6 +17,7 @@ export const LogInPage=()=>{
             {errorMessage && <div className="fail">{errorMessage}</div>}
             <input value={emailValue} onChange={e=> setEmailValue(e.target.value)} placeholder ="someone@gmail.com"/>
             <input value ={passwordValue} onChange = {e=>setPasswordValue(e.target.value)} type="password" placeholder="password"/>
+            <hr></hr>
             <button disabled ={!emailValue || !passwordValue} onClick={onLogInClicked}>Log In</button>
             <button onClick={()=> history.push('/forgot-password')}>Forgot your password?</button>
             <button onClick={()=> history.push('/signup')}>Don't have an account? Sign Up</button>
--- front-end/src/pages/SignUpPage.js
+++ front-end/src/pages/SignUpPage.js
@@ -14,11 +14,12 @@ export const SignUpPage=()=>{
 
     return (
         <div className="content-container">
-            <h1>Log In</h1>
+            <h1>Sign Up</h1>
             {errorMessage && <div className="fail">{errorMessage}</div>}
             <input value={emailValue} onChange={e=> setEmailValue(e.target.value)} placeholder ="someone@gmail.com"/>
             <input value ={passwordValue} onChange = {e=>setPasswordValue(e.target.value)} type="password" placeholder="password"/>
             <input value ={confirmPasswordValue} onChange = {e=>setConfirmPasswordValue(e.target.value)} type="confirm password" placeholder="password"/>
+            <hr></hr>
             <button disabled ={!emailValue || !passwordValue || passwordValue !== confirmPasswordValue} onClick={onSignUpClicked}>Sign Up</button>
             <button onClick={()=> history.push('/login')}>Already have an account</button>
         </div>
